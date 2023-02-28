74ced0c30efb15050f1f0c49d87581c6f4a22cda
Giorgio-Nocerino <giorgionocerino01@outlook.it>
2023-02-28 13:45:05 +0100

    Edit json sc

---
+++ static/js/edit_json_sc.js
@@ -0,0 +1,27 @@
+const downloadBtn = document.getElementById("download-btn");
+const contentTextArea = document.getElementById("content");
+
+downloadBtn.addEventListener("click", async () => {
+  const filename = "definitions.json";
+  const text = contentTextArea.value;
+  const blob = new Blob([text], { type: "application/json" });
+
+  try {
+    const handle = await window.showDirectoryPicker();
+    const writable = await handle.getFileHandle(filename, { create: true });
+    const stream = await writable.createWritable();
+    await stream.write(blob);
+    await stream.close();
+    console.log(`File saved to ${writable.name}`);
+    alert("Il file definitions.json è stato salvato");
+  } catch (error) {
+    console.error(error);
+    alert("Si è verificato un errore");
+  }
+});
+
+
+
+
+
+
