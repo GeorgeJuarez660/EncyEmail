83764380aa51e003b72869f1f1ed3f5058728bf6
Giorgio-Nocerino <giorgionocerino01@outlook.it>
2023-03-03 16:25:52 +0100

    modified

---
+++ static/js/show_plots_definitions.js
@@ -0,0 +1,59 @@
+function ShowDetails(title){
+
+    let defintion_selected = title;
+
+    let  xhttp = new XMLHttpRequest;
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(xhttp.responseText);
+
+            let plots = response.plots;
+
+            let title = '';
+
+            let photo_plot = '';
+
+            let pre_plot = '';
+
+            let history = '';
+
+            for (let element of plots) {
+
+                console.log(plots);
+
+                if (defintion_selected == element.id) {
+
+                    title += `<h2 class="title">${element.title}</h2>`
+
+                    photo_plot += ``
+
+                    pre_plot += ``
+
+                    history += ``
+                }
+            }
+
+            let res_title = document.getElementById("def_title");
+
+            let res_pp = document.getElementById("flex_section");
+
+            let res_prep = document.getElementById("footer");
+
+            let res_history = document.getElementById("casty")
+
+            res_title.innerHTML = title;
+
+            res_pp.innerHTML = photo_plot;
+
+            res_prep.innerHTML = pre_plot;
+
+            res_history.innerHTML = history;
+
+        }
+    };
+
+    xhttp.open("GET", "/static/json/plots_definitions.json", true);
+    xhttp.send();
+
+}
