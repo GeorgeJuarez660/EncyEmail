14d2d2392aff3728f2a60ad9dc0e10b3ac25cd27
Giorgio-Nocerino <giorgionocerino01@outlook.it>
2023-03-03 17:46:16 +0100

    move RL

---
+++ static/js/move_RL.js
@@ -0,0 +1,10 @@
+function move_right(){
+    //Move the viewing window to the right by 500 px
+    console.log('è stato cliccato');
+    document.documentElement.scrollLeft += 1712;
+}
+
+function move_left(){
+    //Move the viewing window to the right by 500 px
+    document.documentElement.scrollLeft += 0;
+}
